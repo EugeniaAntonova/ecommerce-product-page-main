b868955af8627d82a43b5835e4201c15ee86adc1
--replace-all <alexocean.e.k@gmail.com>
2024-06-19 18:00:09 +0500

    feat cart filling and positioning

--- main.js
+++ main.js
@@ -1,108 +1,197 @@
 // ======================================================================== slider
-
-function createSldier(specifier = '', activeIndex = 0) {
-    const sliderWrapper = document.querySelector(`.hero__images-wrapper${specifier}`);
-    const slider = sliderWrapper.querySelector('.hero__images-inner');
-    const sliderControls = [...sliderWrapper.querySelectorAll('.button--slider')];
-    const thumbnails = [...sliderWrapper.querySelectorAll('.button--thumbnail')];
-    const slides = [...slider.querySelectorAll('div')];
-    const lightbox = document.querySelector('.light-box');
-    const closeButton = lightbox.querySelector('.close-lighbox-button');
-    let slideLength = slider.scrollWidth / slides.length;
-
-    const scroll = (evt) => {
-        let left = evt.currentTarget.classList.contains('left');
-        let scrollDirection = left ? -slideLength : slideLength;
-        slider.scrollLeft += scrollDirection;
-        let farLeft = slider.scrollLeft == 0;
-        let farRight = slider.scrollLeft == slider.scrollWidth - slideLength;
-        if (farLeft && left) {
-            slider.scrollLeft = slider.scrollWidth - slideLength;
+(function () {
+    let slideNumber = 0;
+    class Slider {
+        constructor(name, index) {
+            this.sliderWrapper = document.querySelector(name);
+            this.slider = this.sliderWrapper.querySelector('.hero__images-inner');
+            this.sliderControls = [...this.sliderWrapper.querySelectorAll('.button--slider')];
+            this.thumbnails = [...this.sliderWrapper.querySelectorAll('.button--thumbnail')];
+            this.slides = [...this.slider.querySelectorAll('div')];
+            this.slideLength = this.slider.scrollWidth / this.slides.length;
+            this.activeThumbnail = this.thumbnails[index];
+            this.scrollFunc = this.scroll.bind(this);
+            this.onThumbnailClickFunc = this.onThumbnailClick.bind(this);
+            this.onSliderScrollFunc = this.onSliderScroll.bind(this);
         }
-        if (farRight && !left) {
-            slider.scrollLeft = 0;
+
+        scroll(evt) {
+            let left = evt.currentTarget.classList.contains('left');
+            let scrollDirection = left ? -this.slideLength : this.slideLength;
+            this.slider.scrollLeft += scrollDirection;
+            let farLeft = this.slider.scrollLeft == 0;
+            let farRight = this.slider.scrollLeft == this.slider.scrollWidth - this.slideLength;
+            if (farLeft && left) {
+                this.slider.scrollLeft = this.slider.scrollWidth - this.slideLength;
+            }
+            if (farRight && !left) {
+                this.slider.scrollLeft = 0;
+            }
+        }
+
+        activateSliderControls() {
+            this.sliderControls.forEach((item) => { item.addEventListener('click', this.scrollFunc) });
+        }
+
+        deactivateSliderControls() {
+            this.sliderControls.forEach((item) => { item.removeEventListener('click', this.scrollFunc) });
+        }
+
+        initializeThumbnail() {
+            this.activeThumbnail.classList.add('active');
+            this.slides[this.thumbnails.indexOf(this.activeThumbnail)].scrollIntoView();
+        }
+
+        replaceActiveThumbnail(currentThumbnail) {
+            this.activeThumbnail.classList.remove('active');
+            currentThumbnail.classList.add('active');
+            this.activeThumbnail = currentThumbnail;
+        }
+
+        onThumbnailClick(evt) {
+            let thumbnail = evt.currentTarget;
+            this.replaceActiveThumbnail(thumbnail);
+            this.slides[this.thumbnails.indexOf(thumbnail)].scrollIntoView();
+        }
+
+        activateThumbnails() {
+            this.thumbnails.forEach((thumbnail) => {
+                thumbnail.addEventListener('click', this.onThumbnailClickFunc);
+            })
+        }
+        deactivateThumbnails() {
+            this.thumbnails.forEach((thumbnail) => {
+                thumbnail.removeEventListener('click', this.onThumbnailClickFunc);
+            })
+        }
+
+        onSliderScroll() {
+            this.replaceActiveThumbnail(this.thumbnails[this.slider.scrollLeft / this.slideLength]);
+        }
+
+        syncronizeScroll() {
+            this.slider.addEventListener('scrollend', this.onSliderScrollFunc);
+        }
+        unsyncronizeScroll() {
+            this.slider.removeEventListener('scrollend', this.onSliderScrollFunc);
         }
     }
 
-    let activeThumbnail = thumbnails[activeIndex];
-    console.log(activeIndex);
-    console.log(activeThumbnail);
+    document.addEventListener('DOMContentLoaded', () => {
+        let test = new Slider('.hero__images-wrapper', 0);
+        if (window.innerWidth < 850) {
+            test.activateSliderControls();
+            return;
+        }
+        test.initializeThumbnail();
+        test.activateThumbnails();
+        test.syncronizeScroll();
 
-    const replaceActiveThumbnail = (currentThumbnail) => {
-        console.log('repl');
-        activeThumbnail.classList.remove('active');
-        currentThumbnail.classList.add('active');
-        activeThumbnail = currentThumbnail;
-    }
-
-    thumbnails.forEach((thumbnail) => {
-        thumbnail.addEventListener('click', () => {
-            replaceActiveThumbnail(thumbnail);
-            slides[thumbnails.indexOf(thumbnail)].scrollIntoView();
+        window.addEventListener('resize', () => {
+            window.innerWidth < 850 ? test.activateSliderControls() : test.deactivateSliderControls();
+            window.innerWidth < 850 ? test.deactivateThumbnails() : test.activateThumbnails();
+            window.innerWidth < 850 ? test.unsyncronizeScroll() : test.syncronizeScroll();
         })
     })
 
-    sliderControls.forEach((item) => { item.addEventListener('click', scroll) });
 
-    // const observer = new IntersectionObserver((entries) => {
-    //     entries.forEach((entry) => {
-    //         console.log(entry.isIntersecting);
-    //         let index = slides.indexOf(entry.target);
-    //         console.log(index);
-    //         replaceActiveThumbnail(thumbnails[index]);
-    //     })
-    // }, {
-    //     threshold: 1,
-    // });
-
-    // slides.forEach((slide) => {
-    //     observer.observe(slide);
-    // })
-
-    slider.addEventListener('scrollend', () => {
-        replaceActiveThumbnail(thumbnails[slider.scrollLeft / slideLength]);
-    })
-
-    const handleEsc = (evt) => {
-        if (evt.key === 'Escape') closeLightbox();
-    }
-    const handleSideClick = (evt) => {
-        if (evt.target.classList.contains('light-box')) closeLightbox();
-    }
-
-    const closeLightbox = () => {
-        lightbox.classList.remove('open');
-        closeButton.removeEventListener('click', closeLightbox);
-        lightbox.removeEventListener('click', handleSideClick);
-        document.removeEventListener('keydown', handleEsc);
-    }
-
-    const openLightbox = (index) => {
-        lightbox.classList.add('open');
-        closeButton.addEventListener('click', closeLightbox);
-        document.addEventListener('keydown', handleEsc);
-        lightbox.addEventListener('click', handleSideClick);
-        createSldier('.light-box-slider', index);
-    }
-
-    const onSlideClick = (evt) => {
-        openLightbox(slides.indexOf(evt.target.closest('div')));
-    }
-
-    if (!specifier) {
-        slider.addEventListener('click', onSlideClick)
-    }
-
-    window.addEventListener('resize', () => {
-        slideLength = slider.scrollWidth / [...slider.children].length;
-    })
+    let test2 = new Slider('.hero__images-wrapper.light-box-slider', 3);
+    test2.activateSliderControls();
+    test2.initializeThumbnail();
+    test2.activateThumbnails();
+    test2.syncronizeScroll();
 }
+)()
+
+// function createSldier(specifier = '', activeIndex = 0) {
+//     const sliderWrapper = document.querySelector(`.hero__images-wrapper${specifier}`);
+//     const slider = sliderWrapper.querySelector('.hero__images-inner');
+//     const sliderControls = [...sliderWrapper.querySelectorAll('.button--slider')];
+//     const thumbnails = [...sliderWrapper.querySelectorAll('.button--thumbnail')];
+//     const slides = [...slider.querySelectorAll('div')];
+//     // const lightbox = document.querySelector('.light-box');
+//     // const closeButton = lightbox.querySelector('.close-lighbox-button');
+//     let slideLength = slider.scrollWidth / slides.length;
+
+//     const scroll = (evt) => {
+//         let left = evt.currentTarget.classList.contains('left');
+//         let scrollDirection = left ? -slideLength : slideLength;
+//         slider.scrollLeft += scrollDirection;
+//         let farLeft = slider.scrollLeft == 0;
+//         let farRight = slider.scrollLeft == slider.scrollWidth - slideLength;
+//         if (farLeft && left) {
+//             slider.scrollLeft = slider.scrollWidth - slideLength;
+//         }
+//         if (farRight && !left) {
+//             slider.scrollLeft = 0;
+//         }
+//     }
+
+//     let activeThumbnail = thumbnails[0];
+
+//     const replaceActiveThumbnail = (currentThumbnail) => {
+//         activeThumbnail.classList.remove('active');
+//         currentThumbnail.classList.add('active');
+//         activeThumbnail = currentThumbnail;
+//     }
+
+//     // replaceActiveThumbnail(thumbnails[activeIndex]);
+//     // slides[thumbnails[activeIndex]].scrollIntoView();
+
+//     thumbnails.forEach((thumbnail) => {
+//         thumbnail.addEventListener('click', () => {
+//             replaceActiveThumbnail(thumbnail);
+//             slides[thumbnails.indexOf(thumbnail)].scrollIntoView();
+//         })
+//     })
+
+//     sliderControls.forEach((item) => { item.addEventListener('click', scroll) });
+
+//     slider.addEventListener('scrollend', () => {
+//         replaceActiveThumbnail(thumbnails[slider.scrollLeft / slideLength]);
+//     })
+
+//     const handleEsc = (evt) => {
+//         if (evt.key === 'Escape') closeLightbox();
+//     }
+//     const handleSideClick = (evt) => {
+//         if (evt.target.classList.contains('light-box')) closeLightbox();
+//     }
+
+//     const closeLightbox = () => {
+//         lightbox.classList.remove('open');
+//         closeButton.removeEventListener('click', closeLightbox);
+//         lightbox.removeEventListener('click', handleSideClick);
+//         document.removeEventListener('keydown', handleEsc);
+//     }
+
+//     const openLightbox = (index) => {
+//         lightbox.classList.add('open');
+//         closeButton.addEventListener('click', closeLightbox);
+//         document.addEventListener('keydown', handleEsc);
+//         lightbox.addEventListener('click', handleSideClick);
+//         createSldier('.light-box-slider', index);
+//     }
+
+//     const onSlideClick = (evt) => {
+//         openLightbox(slides.indexOf(evt.target.closest('div')));
+//     }
+
+//     if (!specifier) {
+//         slider.addEventListener('click', onSlideClick)
+//     }
+
+//     window.addEventListener('resize', () => {
+//         slideLength = slider.scrollWidth / [...slider.children].length;
+//     })
+// }
 
 
 // ========================================================================= burger and cart
 
 const burger = document.querySelector('.burger');
 const cartControl = document.querySelector('.cart__control');
+const cart = document.querySelector('#cart');
 
 const closePopover = (evt) => {
     if (evt.target.classList.contains('backdrop')) {
@@ -112,7 +201,29 @@ const closePopover = (evt) => {
     }
 }
 
-const onControlerClick = (evt) => {
+const positionCart = (evt) => {
+    let target = evt.currentTarget;
+    let coordinates = target.getBoundingClientRect();
+
+    let top = coordinates.bottom + cart.offsetHeight + 30;
+
+    let left = coordinates.left + (target.offsetWidth - cart.offsetWidth) / 2;
+
+    cart.style.top = `${top}px`;
+    cart.style.left = `${left}px`;
+
+    let right = coordinates.right + (cart.offsetWidth - target.offsetWidth) / 2;
+    if (right > window.innerWidth) {
+        right = 8;
+    }
+
+    if (right == 8) {
+        cart.style.left = `auto`;
+        cart.style.right = `${right}px`;
+    }
+}
+
+const onControlerClick = (evt, isCart) => {
     evt.preventDefault();
     let target = evt.currentTarget;
     let expanded = target.getAttribute('aria-expanded') == 'true' ? true : false;
@@ -120,12 +231,17 @@ const onControlerClick = (evt) => {
 
     if (!expanded) {
         document.addEventListener('click', closePopover);
+        if (isCart) {
+            positionCart(evt)
+        }
     } else {
         document.removeEventListener('click', closePopover);
     }
-
 }
 
+const onCartClick = (evt) => {
+    onControlerClick(evt, true);
+}
 
 function onWindowSize() {
     if (window.innerWidth < 800) {
@@ -134,8 +250,8 @@ function onWindowSize() {
         burger.removeEventListener('click', onControlerClick);
     }
 }
-cartControl.addEventListener('click', onControlerClick);
-createSldier();
+
+cartControl.addEventListener('click', onCartClick);
 
 document.addEventListener('DOMContentLoaded', onWindowSize);
 window.addEventListener('resize', onWindowSize);
@@ -145,19 +261,47 @@ window.addEventListener('resize', onWindowSize);
 // =========================================================================
 
 const form = document.querySelector('.purchase-form');
-const input = form.querySelector('input[name="amount"]');
+const amountInput = form.querySelector('input[name="amount"]');
+const priceInput = form.querySelector('input[name="price"]');
+const nameInput = form.querySelector('input[name="name"]');
+const imgInput = form.querySelector('input[name="picture"]');
 const amountControls = [...form.querySelectorAll('.button--amount')];
+const cartItemTemplate = document.querySelector('.cart-content-template').content;
+const cartList = document.querySelector('.cart__list');
+const cartContent = document.querySelector('.cart__content');
+const filler = document.querySelector('.js-filler');
+const cartCounter = document.querySelector('.cart__counter');
 
-input.addEventListener('keydown', (evt) => {
+amountInput.addEventListener('keydown', (evt) => {
     if (!evt.key.match(/\d|delete|backspace|arrow.+|tab|enter/i)) evt.preventDefault();
 })
 
-// here i do not have any back, so to show the functionality, i have chosen to use session storage
+// here i do not have any backend, so to show the functionality, i have chosen to use session and local storage
 
 let purchaseAmount = Number(sessionStorage.getItem('purchase amount')) || 0;
 
+function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
+
 document.addEventListener('DOMContentLoaded', () => {
-    input.value = purchaseAmount;
+    amountInput.value = purchaseAmount;
+    
+    let previousCartItems = JSON.parse(sessionStorage.getItem('contents'));
+    let empty = isEmpty(previousCartItems);
+    if (!empty) {
+        let total = 0;
+        for (let key in previousCartItems) {
+            let cartItem = createCartItem(previousCartItems[key]);
+            total += previousCartItems[key].amount;
+            cartList.append(cartItem);
+            showCartContents();
+            cartCounter.textContent = total;
+        }
+    }
 })
 
 amountControls.forEach((item) => {
@@ -167,7 +311,55 @@ amountControls.forEach((item) => {
             less = 0;
         }
         purchaseAmount += evt.currentTarget.id === 'less' ? less : 1;
-        input.value = purchaseAmount;
+        amountInput.value = purchaseAmount;
         sessionStorage.setItem('purchase amount', purchaseAmount)
     })
 })
+
+const items = {};
+
+const createCartItem = (data) => {
+    const cartItem = cartItemTemplate.cloneNode(true);
+
+    cartItem.querySelector('.product-name').textContent = data.name;
+    cartItem.querySelector('.product-price').textContent = `$${data.price} x ${data.amount}`;
+    cartItem.querySelector('.product-sum').textContent = `$${parseInt(data.price) * parseInt(data.amount)}`;
+    cartItem.querySelector('.cart__img').src = data.src;
+    cartItem.querySelector('.cart__img').alt = data.name;
+
+    return cartItem;
+}
+
+function showCartContents() {
+    cartContent.classList.remove('hidden');
+    filler.classList.add('hidden');
+}
+
+const onFormSubmit = (evt) => {
+    evt.preventDefault();
+    const src = imgInput.value;
+    const name = nameInput.value;
+    const price = parseInt(priceInput.value);
+    const amount = parseInt(amountInput.value);
+
+    let itemName = name;
+    if (itemName in items) {
+        items[itemName].amount = amount;
+    } else {
+        items[itemName] = { src, name, price, amount };
+    }
+
+    cartList.innerHTML = '';
+
+    for (let key in items) {
+        let cartItem = createCartItem(items[key]);
+        cartList.append(cartItem);
+    }
+
+    cartCounter.textContent = amount;
+    showCartContents();
+
+    sessionStorage.setItem('contents', JSON.stringify(items));
+}
+
+form.addEventListener('submit', onFormSubmit);
